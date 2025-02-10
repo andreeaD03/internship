cf7f180ce4224b6a142dc5ce94cbbe4340290257
unknown <andreeadan0302@gmail.com>
2025-02-10 04:25:01 +0200

    main-description section finished

--- internship-app/src/components/mainDiscription/MainDiscription.jsx
+++ internship-app/src/components/mainDiscription/MainDiscription.jsx
@@ -5,19 +5,11 @@ const MainDiscription = () => {
   return (
     <div>
         <div className='mainDiscription-container'>
-            <div>
                 <h1>Pricing plans</h1>
-            </div>
 
-            <div>
                 <p className='description'>  Lorem ipsum dolor, sit amet
-                    consectetur adipisicing elit
-                    . Cupiditate iste laboriosam
-                    autem, quae corporis enim qu
-                    idem veniam natus cumque! Ut
-                    , perferendis molestiae.
+                    consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Labor
                 </p>
-            </div>
 
         </div>
         
